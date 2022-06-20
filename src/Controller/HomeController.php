f95141844efdd540e47f7897e94ffcd9bf25f376
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-06-20 17:28:03 +0000

    EDIWT

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-		return $this->redirectToRoute('app_participation_new',[],Response::HTTP_SEE_OTHER);
-        /*return $this->render('home/index.html.twig', [
+		#return $this->redirectToRoute('app_participation_new',[],Response::HTTP_SEE_OTHER);
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-        ]);*/
+        ]);
     }
 }
